dbefb5079ac751b448a314b6b1ab496c2983156e
egigoka <egigoka@gmail.com>
2026-07-08 14:26:45 +0500

    Add NotePlus livecheck

--- Casks/noteplus.rb
+++ Casks/noteplus.rb
@@ -7,6 +7,14 @@ cask "noteplus" do
   desc "Native Markdown notes, checklists, and AI chat client"
   homepage "https://noteplus.com/"
 
+  livecheck do
+    url "https://noteplus.com/osx/version.xml"
+    regex(/shortVersionString="v?(\d+(?:\.\d+)+)"/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0] }
+    end
+  end
+
   depends_on macos: :catalina
 
   app "NotePlus.app"
