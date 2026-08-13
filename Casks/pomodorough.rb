f33ae0bfa4a0f80e6b6da87aa5810c0cc5480bb1
egigoka <egigoka@gmail.com>
2026-08-13 14:39:56 +0500

    fix: migrate Pomodorough Apple cask

--- Casks/pomodorough.rb
+++ Casks/pomodorough.rb
@@ -2,18 +2,19 @@ cask "pomodorough" do
   version "1.0,e938939bcf34b32d37166c757e71bb7d8083fb89"
   sha256 "02604bd29b39fe48c294c90b014bcf5958be8374a9d84af600b95ab7b883f312"
 
-  url "https://github.com/egigoka/pomodorough-ios/archive/#{version.csv.second}.tar.gz?version=#{version.csv.first}"
+  url "https://github.com/Pomodoro-Everywhere/pomodorough-apple/archive/#{version.csv.second}.tar.gz?version=#{version.csv.first}"
   name "Pomodorough"
   desc "Pomodoro timer for Apple platforms"
-  homepage "https://github.com/egigoka/pomodorough-ios"
+  homepage "https://github.com/Pomodoro-Everywhere/pomodorough-apple"
 
   livecheck do
-    url "https://api.github.com/repos/egigoka/pomodorough-ios/commits/main"
+    url "https://api.github.com/repos/Pomodoro-Everywhere/pomodorough-apple/commits/main"
     strategy :json do |json|
       "1.0,#{json["sha"]}"
     end
   end
 
+  depends_on arch: :arm64
   depends_on macos: :sonoma
 
   app "Pomodorough.app"
@@ -21,10 +22,11 @@ cask "pomodorough" do
   preflight do
     system_command "/usr/bin/xcodebuild",
                    args:         [
-                     "-project", "#{staged_path}/pomodorough-ios-#{version.csv.second}/Pomodorough.xcodeproj",
+                     "-project", "#{staged_path}/pomodorough-apple-#{version.csv.second}/Pomodorough.xcodeproj",
                      "-scheme", "Pomodorough-macOS",
                      "-configuration", "Release",
                      "-derivedDataPath", "#{staged_path}/build",
+                     "ARCHS=arm64",
                      "CODE_SIGN_IDENTITY=-",
                      "CODE_SIGNING_REQUIRED=NO",
                      "build"
@@ -37,7 +39,7 @@ cask "pomodorough" do
     system_command "/bin/rm",
                    args: [
                      "-rf",
-                     "#{staged_path}/pomodorough-ios-#{version.csv.second}",
+                     "#{staged_path}/pomodorough-apple-#{version.csv.second}",
                      "#{staged_path}/build",
                    ]
   end
